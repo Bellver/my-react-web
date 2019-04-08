33af0bb7127c6e2f73be7dcd88dcd408a8ddbd04
Pablo Bellver Lynx <bellverlynx@MacBook-Pro-Bellver.local>
2019-04-08 10:35:12 +0200

    Change message on button

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,10 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Pablo Bellver</h1>
+    <p>I’m Pablo Bellver, a thirty-three-year-old designer from Spain currently living in Alicante. I’m also the Lead Designer at LynxView</p>
+    <p>Growing my unicorn horn</p>
+    <Link to="/page-2/">See showreal</Link>
   </div>
 )
 
