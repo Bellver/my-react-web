c967ad72d5f8bc18eadf57defc3c6cbcc7636a31
Pablo Bellver <pablobellver.design@gmail.com>
2019-08-17 12:53:53 +0200

    Added animation to hero text and button

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import './Buttons.css'
+
+
+const Button = props => (
+  <div className="Button">
+     <h3>button test</h3>
+  </div>
+)
+
+export default Button
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,9 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
         <div className="HeroGroup">
-          <h1>Hello 👋 I'm Pablo Bellver</h1>
+          <h1>Hello 👋 <br />I'm Pablo Bellver</h1>
           <p>A thirty-three-year-old designer from Spain currently living in Alicante. I’m also Lead Designer at LynxView, but I'm here growing my unicorn horn.</p>
-          <Link to="/work">See my works</Link>
+          <Link to="/contact"><button>Email Me</button></Link>
         </div>
     </div>
   </div>
